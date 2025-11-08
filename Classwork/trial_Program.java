2a995056dc6cc735e01fbd507b3c8803d9096799
pansareprajwal <153760375+pansareprajwal@users.noreply.github.com>
2025-11-08 11:58:08 +0530

    Update

---
+++ Classwork/trial_Program.java
@@ -0,0 +1,9 @@
+class trial_Program
+{
+    public static void main(String A[])
+    {   
+       System.out.println("This trial code ");            
+       System.out.println("This trial code ");            
+       System.out.println("This trial code ");            
+    }
+}
